bea753530d6dcdba3394e9bb93136df806dad115
rolo555 <rolo555@gmail.com>
2011-01-13 01:43:44 -0400

    Primer commit, proyecto vacio; solo 2 plugins activescaffol y paperclip .gitignore creado

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = 'cfd788a93bb07449085249e11ca9207a200e4014799359502ff01b5a4fb7e1b134f916140f8916445a424936daa797551b2e20cb724ec335b589d45da6486c86';
