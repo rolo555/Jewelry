5d4b837aeae40032f84a5ce987594b4ea1dc6cfe
Jesús Mercado <jesusmmm@gmail.com>
2011-01-29 19:29:15 -0400

    Configurar activescaffold para jewelry
    - Agregar box a las columnas del listar
    - Descativar enlace que abre el nested de box
    - Activar busqueda avanzada

--- app/controllers/jewelries_controller.rb
+++ app/controllers/jewelries_controller.rb
@@ -1,6 +1,12 @@
 class JewelriesController < ApplicationController
   active_scaffold :jewelrie do |conf|
     conf.columns = :box, :weight, :description, :photo, :purchase_date, :purchase_price
-    conf.list.columns = :photo, :weight
+    conf.list.columns = :photo, :product_auto_code, :box
+
+    #Descativar enlace que abre el nested
+    conf.columns[:box].clear_link
+
+    #Activar busqueda avanzada
+    conf.actions << :field_search
   end
 end
