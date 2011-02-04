94472eb4af823c019fd5bc0937cb5ff057afc5f8
rolo555 <rolo555@gmail.com>
2011-02-03 20:33:46 -0400

    cambio de nombre de variable a options

--- app/helpers/incomes_helper.rb
+++ app/helpers/incomes_helper.rb
@@ -1,5 +1,5 @@
 module IncomesHelper
-  def income_payment_type_search_column(record, input_name)
-    select :record, :payment_type, options_for_select({ as_(:payment) => "Payment", as_(:sale) => "Sale"  }), {:include_blank => as_('- select -')}, input_name
+  def income_payment_type_search_column(record, options)
+    select :record, :payment_type, options_for_select({ as_(:payment) => "Payment", as_(:sale) => "Sale"  }), {:include_blank => as_('- select -')}, options
   end
 end
