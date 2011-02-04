4e0f4aa95c644d09e11ff0ef12d6fefecb2041cf
rolo555 <rolo555@gmail.com>
2011-02-03 20:34:34 -0400

    configuracion de las fechas para que inicie en la actual

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,6 +1,8 @@
 class SalesController < ApplicationController
   active_scaffold :sale do |conf|
     conf.columns = :date_of_sale, :amount
+    #configuracion de la fecha de venta
+    conf.columns[:date_of_sale].options = {:end_year => Date.today.year-5, :start_year => Date.today.year, :include_blank => false }
   end
 
 end
