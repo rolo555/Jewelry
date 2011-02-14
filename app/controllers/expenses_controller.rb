251b7b3d16b1565f5d84714a62e439308cda8514
rolo555 <rolo555@gmail.com>
2011-02-14 19:58:59 -0400

    al listar se muestra payment_date en ves de created_at

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,6 +1,6 @@
 class ExpensesController < ApplicationController
   active_scaffold :expense do |conf|
-    conf.list.columns = :created_at, :concept, :jewelry, :amount
+    conf.list.columns = :payment_date, :concept, :jewelry, :amount
     #sumatoria de amount
     conf.columns[:amount].calculate = :sum
   end
