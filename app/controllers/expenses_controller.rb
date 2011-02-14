62a86dc2534be15b5f3796a6dabb1017e319de9b
rolo555 <rolo555@gmail.com>
2011-02-14 18:41:42 -0400

    agregacion de sumatoria de amount en expenses

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,7 @@
 class ExpensesController < ApplicationController
   active_scaffold :expense do |conf|
     conf.list.columns = :created_at, :concept, :jewelry, :amount
+    #sumatoria de amount
+    conf.columns[:amount].calculate = :sum
   end
 end
