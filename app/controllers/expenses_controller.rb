651d5f3a7eebca534095ba95ad6d636020f3c94e
rolo555 <rolo555@gmail.com>
2011-02-19 07:18:07 -0400

    modificacion de link de jewelry a ver

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -3,5 +3,9 @@ class ExpensesController < ApplicationController
     conf.list.columns = :payment_date, :concept, :jewelry, :amount
     #sumatoria de amount
     conf.columns[:amount].calculate = :sum
+    
+    #modificar el link a ver
+    conf.columns[:jewelry].actions_for_association_links = [:show]
+
   end
 end
