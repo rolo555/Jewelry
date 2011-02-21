43a435728f505fdfa0e0e340b1375b66399e6067
Jesús Mercado <jesusmmm@gmail.com>
2011-02-21 18:57:47 -0400

    Agregar bs y usd, price a sales cotroller

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,6 +1,7 @@
 class SalesController < ApplicationController
   active_scaffold :sale do |conf|
-    conf.columns = :date_of_sale, :amount
+    conf.columns = :date_of_sale, :bs, :usd
+    conf.show.columns = :date_of_sale, :price
     #configuracion de la fecha de venta
     conf.columns[:date_of_sale].options = {:end_year => Date.today.year-5, :start_year => Date.today.year, :include_blank => false }
   end
