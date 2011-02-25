f56beaf0a2e40d51aa30fc951006d1e9a99865eb
rolo555 <rolo555@gmail.com>
2011-02-25 19:07:13 -0400

    to_label de expense

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -9,7 +9,6 @@ class Expense < ActiveRecord::Base
   #Validaciones
   validates_numericality_of :bs, :greater_than_or_equal_to => 0, :if => "self.bs.present?"
   validates_numericality_of :usd, :greater_than_or_equal_to => 0, :if => "self.usd.present?"
-  validates_numericality_of :amount, :greater_than_or_equal_to => 0, :if => "self.amount.present?"
 
   #Desactivar delete
   def authorized_for_delete?
@@ -18,5 +17,11 @@ class Expense < ActiveRecord::Base
 
   def amounts=(amounts)
     self.bs, self.usd = amounts
+  end
+
+  def to_label
+    product = jewelry.to_label if jewelry.present?
+    date = I18n.l(payment_date, :format => :long) if payment_date.present?
+    [concept, product, date].join " "
   end
 end
