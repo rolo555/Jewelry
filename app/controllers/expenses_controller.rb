610052fd4b656c3c2008b2d4348c7d753eabce0f
rolo555 <rolo555@gmail.com>
2011-02-25 19:07:38 -0400

    Eliminar jewelry del formulario en expenses

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,7 +1,9 @@
 class ExpensesController < ApplicationController
   active_scaffold :expense do |conf|
-    conf.list.columns = :payment_date, :concept, :jewelry, :bs, :usd
+    conf.columns = :payment_date, :concept, :bs, :usd
 
+    conf.list.columns = :payment_date, :concept, :jewelry, :bs, :usd
+    
     #Sumatoria de bs y usd
     conf.columns[:bs].calculate = :sum
     conf.columns[:usd].calculate = :sum
