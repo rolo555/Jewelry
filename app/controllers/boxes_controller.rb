fa751458be327f85d6122f0cf7bb4012ca508b1f
Jesús Mercado <jesusmmm@gmail.com>
2011-02-26 14:03:27 -0400

    Refactorizar box modelo y controlador

--- app/controllers/boxes_controller.rb
+++ app/controllers/boxes_controller.rb
@@ -1,8 +1,9 @@
 class BoxesController < ApplicationController
   active_scaffold :boxe do |conf|
+    #Configuración de las columnas que se mostrarán en general
     conf.columns = [:photo, :box_code, :description]
 
-    #No mostrar el link delete
+    #Quitar el link en la acción delete
     conf.delete.link = false
   end
 end
--- app/models/box.rb
+++ app/models/box.rb
@@ -1,16 +1,15 @@
 include ModelHelper
 
 class Box < ActiveRecord::Base
-  protected :before_validation
 
   #Relaciones
   belongs_to :product
-  has_many :jewelries
+  has_many :jewelries, :dependent => :destroy
 
   #Validaciones
   validates_presence_of :box_code, :description
-  validates_length_of :box_code, :maximum => 50, :if => "self.box_code.presence"
-  validates_length_of :description, :maximum => 50, :if => "self.description.presence"
+  validates_length_of :box_code, :maximum => 50, :if => "box_code.present?"
+  validates_length_of :description, :maximum => 50, :if => "description.present?"
   validates_uniqueness_of :box_code, :case_sensitive => false
 
   #Fotografia
@@ -18,16 +17,23 @@ class Box < ActiveRecord::Base
     :styles => { :thumbnail => "40x40" },
     :default_url => "missing.png"
 
-  def before_validation
-    sanitizate_strings :box_code, :description
+  #Permisos
+  def authorized_for_delete?
+    false
+  end
+
+  def box_code=(value)
+    value.strip! if value.present?
+    self.write_attribute(:box_code, value)
+  end
+
+  def description=(value)
+    value.strip! if value.present?
+    self.write_attribute :description, value
   end
 
   def to_label
     "#{box_code} - #{description}"
   end
 
-  #Desactivar delete
-  def authorized_for_delete?
-    false
-  end
 end
