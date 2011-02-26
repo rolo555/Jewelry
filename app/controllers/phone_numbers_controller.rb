063b6c7b8a88f3c848e40e6f73342380d39f5eff
Jesús Mercado <jesusmmm@gmail.com>
2011-02-26 18:15:48 -0400

    Refactorizar telephone_number model y controller

--- app/controllers/phone_numbers_controller.rb
+++ app/controllers/phone_numbers_controller.rb
@@ -1,9 +1,11 @@
 class PhoneNumbersController < ApplicationController
+
   active_scaffold :phone_number do |conf|
-    #Configuración de las columnas que se mostrarán
+    #Configuración de las columnas que se mostrarán en general
     conf.columns = [:number]
 
     #Agregar a las columnas de listar la relación deuda
     conf.list.columns.add :debt
   end
+
 end
--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -1,21 +1,19 @@
-include ModelHelper
-
 class PhoneNumber < ActiveRecord::Base
-
-  protected :before_validation
 
   #Relaciones
   belongs_to :debt
 
   #Validaciones
   validates_presence_of :number
-  validates_length_of :number, :maximum => 50, :if => "self.number.presence"
+  validates_length_of :number, :maximum => 50, :if => "number.present?"
+
+  def number=(value)
+    value.strip!
+    self.write_attribute(:number, value)
+  end
 
   def to_label
     number.to_s
   end
 
-  def before_validation
-    sanitizate_strings :number
-  end
 end
