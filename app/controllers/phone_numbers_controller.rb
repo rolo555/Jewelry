94358fa961dc613b6a08821c67fc024b5a08a9dd
Jesús Mercado <jesusmmm@gmail.com>
2011-02-26 22:58:03 -0400

    Refactorizar phone_number controller

--- app/controllers/phone_numbers_controller.rb
+++ app/controllers/phone_numbers_controller.rb
@@ -3,9 +3,6 @@ class PhoneNumbersController < ApplicationController
   active_scaffold :phone_number do |conf|
     #Configuración de las columnas que se mostrarán en general
     conf.columns = [:number]
-
-    #Agregar a las columnas de listar la relación deuda
-    conf.list.columns.add :debt
   end
 
 end
