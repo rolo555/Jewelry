0acd5b26ed5f271a4af3ba3e0e80a2b5423a5183
rolo555 <rolo555@gmail.com>
2011-04-19 14:17:23 -0400

    crear historial de la tabla debt al modificar el numero de telefono

--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -7,6 +7,18 @@ class PhoneNumber < ActiveRecord::Base
   validates_presence_of :number
   validates_length_of :number, :maximum => 50, :if => "number.present?"
 
+  def before_update
+    if self.number_changed?
+      Record.create :table => "Deuda",
+        :code => "Venta de la joya #{self.jewelry.jewelry_code}",
+        :message => "Se modificó el número de telefono.\nAntes era '#{self.number_was}' y ahora es '#{self.number}'."
+    end
+  end
+
+  def jewelry
+    debt.jewelry if debt.present?
+  end
+
   def number=(value)
     value.strip!
     self.write_attribute(:number, value)
